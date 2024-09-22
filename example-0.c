a4449bd5b5e2dcea2b24c0f9de334a50932af4d7
MosesDVarghese <accydent@gmail.com>
2024-09-22 20:40:20 +0400

    init

---
+++ example-0.c
@@ -0,0 +1,32 @@
+// run
+// gcc -o app $(pkg-config --cflags --libs gtk4) example-0.c && ./app
+// gcc $( pkg-config --cflags gtk4 ) -o example-0 example-0.c $( pkg-config --libs gtk4 )
+
+#include <gtk/gtk.h>
+
+static void
+activate (GtkApplication* app,
+          gpointer        user_data)
+{
+  GtkWidget *window;
+
+  window = gtk_application_window_new (app);
+  gtk_window_set_title (GTK_WINDOW (window), "Window");
+  gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
+  gtk_window_present (GTK_WINDOW (window));
+}
+
+int
+main (int    argc,
+      char **argv)
+{
+  GtkApplication *app;
+  int status;
+
+  app = gtk_application_new ("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
+  g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
+  status = g_application_run (G_APPLICATION (app), argc, argv);
+  g_object_unref (app); // free window from memory upon quitting
+
+  return status;
+}
